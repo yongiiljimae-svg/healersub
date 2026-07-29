1befb9c342fe3b1a91b1bc68a806f5663c011fdb
yongiiljimae-svg <yongiiljimae@gmail.com>
2026-07-29 22:54:44 +0330

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -6,6 +6,6 @@
 ===================================================== */
 
 const SUPABASE_URL = "https://adiozydhczsqrpjgcxcc.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_y6_lpA_SiWXioaHTSib1mw_sQuIZHok";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkaW96eWRoY3pzcXJwamdjeGNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMzgzOTEsImV4cCI6MjEwMDkxNDM5MX0.fwgSLCtetkpjPJOWXV6rsmWoEESJg4ovzYkwyy2i14c";
 
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
